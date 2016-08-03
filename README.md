6d55e6ae276231d9243857ef013d4c50d26c89bf
HakaseShounen <hakaseshounen@gmail.com>
2016-08-04 01:39:37 +0800

    Done 0.1v

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # ClearRAM
 Clear RAM Memory Cache, Buffer and Swap Space on Linux
+
+## Synopsis
+This code will clear your ram memory cache, buffer and swap space on Linux.
+
+## How to add to my bashrc ?
+Simple follow this step.
+
+Pull this repo first !
+		git pull https://github.com/HakaseShounen/ClearRAM
+
+Go to repo directory
+		cd ClearRAM
+
+Add to bashrc
+		cat code.sh >> ~/.bashrc
+
+Done
+
+## Screenshot
+Coming soon!
+
+## Copyright
+Copyright (c) 2016 Hakase Shounen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+## Contact
+Feel free to contact me at hakaseshounen@gmail.com
---
+++ code.sh
@@ -0,0 +1,40 @@
+# Written by HakaseShounen
+# August 04, 2016
+# Feel free to contact me at: hakaseshounen@gmail.com
+#
+# References:
+# http://unix.stackexchange.com/questions/269078/executing-a-bash-script-function-with-sudo
+# http://www.tecmint.com/clear-ram-memory-cache-buffer-and-swap-space-on-linux/
+# http://askubuntu.com/questions/155768/how-do-i-clean-or-disable-the-memory-cache
+
+
+# To check current memory usage, run this:
+#	$	watch -n 1 free -m
+#			or
+#	$	watch -n 1 cat /proc/meminfo
+clear_RAM() {
+	if [[ $EUID -ne 0 ]]; then
+		echo "You must be a root user"
+		echo "$ sudo bash $0"
+		exit 1
+	else
+# [soon]	#total="$(free -h | sed -n -e '2p' | awk '{print $2}')"
+# [soon]	#free="$(free -h | sed -n -e '2p' | grep -Po '\S+$')"
+# [soon]	#total_ram_before="$(free -m | grep 'Mem:' | awk '{print $2}')"
+# [soon]	#free_ram_before="$(free -m | grep 'Mem:' | awk '{print $7}')"
+# [soon]	#echo "	Before:"
+# [soon]	#echo "		Total RAM: ${total_ram_before}"
+# [soon]	#echo "		Currently available RAM: ${free_ram_before}"
+	echo " "
+	# clearing..
+	echo " "
+	su -c "echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'" root
+# [soon]	#total_ram_after="$(free -m | grep 'Mem:' | awk '{print $2}')"
+# [soon]	#free_ram_after="$(free -m | grep 'Mem:' | awk '{print $7}')"
+# [soon]	#echo "	After:"
+# [soon]	#echo "		Total RAM: ${total_ram_after}"
+# [soon]	#echo "		Currently available RAM: ${free_ram_after}"
+	fi	
+}
+FREE_RAM=$(declare -f clear_RAM)
+alias freeram='sudo bash -c "$FREE_RAM; clear_RAM"'
